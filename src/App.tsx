85903fa9cbe16d34d6f092e9b1f244bc8764d509
Sushants-Git <sushantsgml@gmail.com>
2024-10-06 23:16:24 +0530

    add tailwind & dotted background

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
+import DottedBackground from "./components/DottedBackground";
 import { useWindowTitleStream } from "./hooks/useWindowTitleStream";
 
 function App() {
     const { title, isStreamRunning, changeStreamStatus } = useWindowTitleStream();
 
     return (
-        <div className="container">
-            <h2>Current Window Title:</h2>
-            <p>{title || "No title streaming"}</p>
-            <button onClick={changeStreamStatus}>
-                {isStreamRunning() ? "Stop Stream" : "Start Stream"}
-            </button>
-        </div>
+        <DottedBackground>
+            <div>
+            Hello
+            </div>
+        </DottedBackground>
     );
 }
 
 export default App;
+
+// return (
+//     <DottedBackground>
+//     <div className="container">
+//     <h2>Current Window Title:</h2>
+//     <p>{title || "No title streaming"}</p>
+//     <button onClick={changeStreamStatus}>
+//     {isStreamRunning() ? "Stop Stream" : "Start Stream"}
+//     </button>
+//     </div>
+//     </DottedBackground>
+// );
