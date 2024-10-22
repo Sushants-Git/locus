6373ee7734a7de9a66312b3f51d8df809f3ccbc3
Sushants-Git <sushantsgml@gmail.com>
2024-10-22 17:13:10 +0530

    feat: working timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,21 +1,80 @@
 import NumberFlow from "@number-flow/react";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { useTimerStore } from "../stores/settingStore";
+import { Play, Pause, TimerReset } from "lucide-react";
+import { defaults } from "../constants";
+import { useEffect, useState } from "react";
+
+function convertSeconds(totalSeconds: number) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return { minutes, seconds };
+}
 
 export default function Timer() {
     const backgroundImagePath = useTimerStore(state => state.backgroundImagePath);
+    const accentColor = useTimerStore(state => state.accentColor);
 
-    let minute = 40;
-    let second = 50;
+    const {
+        sessionLengthInSeconds: sessionLength,
+        breakLengthInSeconds: breakLength,
+        numberOfSessions,
+        timerStatus,
+        setTimerStatus,
+    } = useTimerStore();
+
+    const [time, setTime] = useState(sessionLength);
+    const [currentSession, setCurrentSession] = useState(1);
+    const [completedAllSessions, setCompletedAllSessions] = useState(false);
+
+    const handleReset = () => {
+        setTimerStatus("idle");
+        setTime(sessionLength);
+        setCurrentSession(1);
+        setCompletedAllSessions(false);
+    };
+
+    let iconColor = accentColor || defaults.accentColor;
+    let { minutes, seconds } = convertSeconds(time);
+
+    if (timerStatus === "running" && time <= 0) {
+        if (currentSession > numberOfSessions) {
+            setTimerStatus("completed");
+            setCompletedAllSessions(true);
+        } else {
+            setTimerStatus("break");
+            setTime(breakLength);
+            setCurrentSession(done => done + 1);
+        }
+    }
+
+    if (timerStatus === "break" && time <= 0) {
+        setTimerStatus("running");
+        setTime(sessionLength);
+    }
+
+    useEffect(() => {
+        let id = null;
+
+        if (timerStatus === "running" || timerStatus === "break") {
+            id = setInterval(() => {
+                setTime(time => time - 1);
+            }, 1000);
+        }
+
+        return () => {
+            id && clearInterval(id);
+        };
+    }, [timerStatus]);
 
     return (
-        <div className="font-bricolage-grotesque mb-5 mt-5">
+        <div className="font-bricolage-grotesque mb-5 mt-5 flex justify-center gap-4">
             <div
-                className="text-9xl tabular-nums text-white font-600 m-auto w-fit border rounded-lg px-8 relative group select-none"
+                className="text-9xl tabular-nums text-white font-600 w-fit border rounded-lg px-8 relative group select-none"
                 style={{
                     backgroundImage: backgroundImagePath
                         ? `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url(${convertFileSrc(backgroundImagePath)})`
-                        : "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url(/background/background.png)",
+                        : `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url(${defaults.backgroundImagePath})`,
 
                     backgroundRepeat: "no-repeat",
                     backgroundPosition: "center",
@@ -23,18 +82,51 @@ export default function Timer() {
                 }}
             >
                 <NumberFlow
-                    value={minute}
+                    value={minutes}
                     format={{ notation: "compact" }}
                     isolate
                     className="[--number-flow-char-height:0.80em]"
                 />
                 :
                 <NumberFlow
-                    value={second}
+                    value={seconds}
                     format={{ notation: "compact", minimumIntegerDigits: 2 }}
                     isolate
                     className="[--number-flow-char-height:0.80em]"
                 />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-2">
+                {timerStatus !== "completed" && (
+                    <div>
+                        {timerStatus === "idle" || timerStatus === "paused" ? (
+                            <Play
+                                className="h-4 w-4 cursor-pointer"
+                                fill={iconColor}
+                                stroke={iconColor}
+                                onClick={() => setTimerStatus("running")}
+                            />
+                        ) : (
+                            <Pause
+                                className="h-4 w-4 cursor-pointer"
+                                fill={iconColor}
+                                stroke={iconColor}
+                                onClick={() => setTimerStatus("paused")}
+                            />
+                        )}
+                    </div>
+                )}
+                <div style={{ color: iconColor }}>
+                    {currentSession}/{numberOfSessions}
+                </div>
+                {timerStatus !== "idle" && (
+                    <div>
+                        <TimerReset
+                            className="h-4 w-4 cursor-pointer"
+                            stroke={iconColor}
+                            onClick={handleReset}
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/stores/settingStore.tsx
+++ src/stores/settingStore.tsx
@@ -11,13 +11,37 @@ type SettingStoreType = {
 type TimerStoreType = {
     backgroundImagePath: string | null;
     accentColor: string | null;
+
+    sessionLengthInSeconds: number;
+    numberOfSessions: number;
+    breakLengthInSeconds: number;
+    // idle: Before starting any session.
+    // running: Timer is actively counting down for a Pomodoro session.
+    // paused: The session is paused.
+    // break: Timer is on a break (either short or long break).
+    // ended: The current Pomodoro session has ended.
+    // completed: The entire Pomodoro session cycle (e.g., 4 sessions) has been finished.
+    timerStatus: "idle" | "running" | "paused" | "break" | "ended" | "completed";
+
     setBackgroundImagePath: (path: string) => Promise<void>;
     setAccentColor: (hex: string) => Promise<void>;
+    setTimerStatus: (status: TimerStoreType["timerStatus"]) => void;
+    setTimerInitials: (time: {
+        sessionLengthInSeconds?: number;
+        numberOfSessions?: number;
+        breakLengthInSeconds?: number;
+    }) => void;
 };
 
 const useTimerStore = create<TimerStoreType>()(set => ({
     backgroundImagePath: null,
     accentColor: null,
+
+    sessionLengthInSeconds: 25 * 60,
+    numberOfSessions: 2,
+    breakLengthInSeconds: 5 * 60,
+    timerStatus: "idle",
+
     setBackgroundImagePath: async (path: string) => {
         set(() => ({ backgroundImagePath: path }));
         await store.set("timer-background-image-path", path);
@@ -25,6 +49,12 @@ const useTimerStore = create<TimerStoreType>()(set => ({
     setAccentColor: async (hex: string) => {
         set(() => ({ accentColor: hex }));
         await store.set("timer-accent-color", hex);
+    },
+    setTimerStatus: status => {
+        set(() => ({ timerStatus: status }));
+    },
+    setTimerInitials: time => {
+        set(() => ({ ...time }));
     },
 }));
 
@@ -39,12 +69,28 @@ const hydrateSettings = async () => {
 
     const accentColor = z.string().safeParse(await store.get("timer-accent-color"));
 
+    const sessionLength = z.number().safeParse(await store.get("timer-session-length"));
+    const numberOfSessions = z.number().safeParse(await store.get("timer-number-of-sessions"));
+    const breakLength = z.number().safeParse(await store.get("timer-break-length"));
+
     if (backgroundImagePath.success) {
         useTimerStore.setState({ backgroundImagePath: backgroundImagePath.data });
     }
 
     if (accentColor.success) {
         useTimerStore.setState({ accentColor: accentColor.data });
+    }
+
+    if (sessionLength.success) {
+        useTimerStore.setState({ sessionLengthInSeconds: sessionLength.data });
+    }
+
+    if (numberOfSessions.success) {
+        useTimerStore.setState({ numberOfSessions: numberOfSessions.data });
+    }
+
+    if (breakLength.success) {
+        useTimerStore.setState({ breakLengthInSeconds: breakLength.data });
     }
 
     useSettingStore.setState({ _settingHydrate: true });
