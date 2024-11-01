647638fee4403a753d95217b3265fdae2838a86b
Sushants-Git <sushantsgml@gmail.com>
2024-11-01 21:37:39 +0530

    chore : remove comment

--- src/components/DottedBackground.tsx
+++ src/components/DottedBackground.tsx
@@ -4,26 +4,6 @@ type DottedBackgroundProps = {
     children: React.ReactNode;
 };
 
-// const DottedBackground: React.FC<DottedBackgroundProps> = ({ children }) => {
-//     return (
-//         <>
-//             <div className="relative min-h-screen w-full bg-white overflow-hidden">
-//                 <div
-//                     className="absolute inset-0"
-//                     style={{
-//                         backgroundImage: `radial-gradient(circle, rgba(0, 0, 0, 0.2) 0.5px, transparent 1px)`,
-//                         backgroundSize: "16px 16px",
-//                         backgroundPosition: "center center",
-//                         mask: "radial-gradient(circle, black, transparent 70%)",
-//                         WebkitMask: "radial-gradient(circle, black, transparent 70%)",
-//                     }}
-//                 />
-//                 <div className="absolute">{children}</div>
-//             </div>
-//         </>
-//     );
-// };
-
 export default function DottedBackground({ children }: DottedBackgroundProps) {
     const { theme } = useTheme();
 
@@ -45,5 +25,3 @@ export default function DottedBackground({ children }: DottedBackgroundProps) {
         </div>
     );
 }
-
-// export default DottedBackground;
